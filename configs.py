03941851a4d96644ee7fd6e089f956bdfdc7eec9
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-05-08 02:08:06 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -19,8 +19,8 @@ class Config(object):
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
-I Am Permanent File Store Bot❗
-Send me any file and I will save it in my Database. I'm Also works for channel❗ Add me to your channel as Admin with Edit Permission and I will add Save all your Uploaded File in Channel and add Sharable Button Link.
+**I Am Permanent File Store Bot 🤖
+Send me any file and I will save it in my Database. I'm Also works for channel, Add me to your channel as Admin with Edit Permission and I will add Save all your Uploaded File in Channel and add Sharable Button Link.🔗**
 
 🤖 **My Name:** [Doluram Files](https://t.me/{BOT_USERNAME})
 
