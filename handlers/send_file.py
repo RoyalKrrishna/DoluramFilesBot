941e2894d166616be728f468133d7e2504784c5f
Mradul <nirusakimalaal@gmail.com>
2022-05-08 09:46:33 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -10,11 +10,13 @@ from handlers.helpers import str_to_b64
 
 async def reply_forward(message: Message, file_id: int):
     try:
-        await message.reply_text(
+        link = await message.reply_text(
             f"**Here is Sharable Link of this file:**\n"
             f"https://t.me/{Config.BOT_USERNAME}?start=Doluram{str_to_b64(str(file_id))}\n\n"
             f"__To Retrive the Stored File, just open the link!__",
             disable_web_page_preview=True, quote=True)
+            await asyncio.sleep(1800)
+            await link.delete()
     except FloodWait as e:
         await asyncio.sleep(e.x)
         await reply_forward(message, file_id)
