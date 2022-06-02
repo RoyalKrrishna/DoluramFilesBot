1d78d26ed2ded09fbf576bcd568bf20dde2a6e43
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-06-02 23:45:38 +0530

    Create send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -17,7 +17,6 @@ async def reply_forward(message: Message, file_id: int):
             disable_web_page_preview=True, quote=True)
     except FloodWait as e:
         await asyncio.sleep(2)
-        await link.delete()
         await reply_forward(message, file_id)
 
 
@@ -27,16 +26,16 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
             return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                           message_id=file_id)
         elif Config.FORWARD_AS_COPY is False:
-            return
-       final_msg = await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                               message_ids=file_id)
     except FloodWait as e:
         await asyncio.sleep(2)
-        await final_msg.delete()
         return media_forward(bot, user_id, file_id)
 
 
-async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
+async def
+    final_msg = send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await reply_forward(message=sent_message, file_id=file_id)
-    await asyncio.sleep(2)
+    await asyncio.sleep(10)
+    await final_msg.delete()
