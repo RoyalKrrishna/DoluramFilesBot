f0ae27ad1f80c31178847494703eea388841e4ae
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-06-02 23:50:59 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -16,7 +16,8 @@ async def reply_forward(message: Message, file_id: int):
             f"__To Retrive the Stored File, just open the link!__",
             disable_web_page_preview=True, quote=True)
     except FloodWait as e:
-        await asyncio.sleep(2)
+        await asyncio.sleep(10)
+        await link.delete()
         await reply_forward(message, file_id)
 
 
@@ -29,13 +30,11 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
             return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                               message_ids=file_id)
     except FloodWait as e:
-        await asyncio.sleep(2)
+        await asyncio.sleep(e.x)
         return media_forward(bot, user_id, file_id)
 
 
-async def
-    final_msg = send_media_and_reply(bot: Client, user_id: int, file_id: int):
+async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await reply_forward(message=sent_message, file_id=file_id)
     await asyncio.sleep(10)
-    await final_msg.delete()
