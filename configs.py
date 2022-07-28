8ca892e2a8a141a4ea78a2538526b3eb2a9340a4
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-07-28 16:07:45 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,12 +12,12 @@ class Config(object):
 	DATABASE_URL = os.environ.get("DATABASE_URL")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "0")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
-        START_PHOTO = os.environ.get("START_PHOTO")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+        START_PHOTO = os.environ.get("START_PHOTO")
 	ABOUT_BOT_TEXT = f"""
 **I Am Permanent File Store Bot 🤖
 Send me any file and I will save it in my Database.\n\nI'm Also works for channel, Add me to your channel as Admin with Edit Permission and I will Save all your Uploaded File in Channel and add Sharable Button Link.🔗**
