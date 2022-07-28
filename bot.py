652e3c52a7bc079bf9377b995036f79b3f16f052
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-07-28 15:04:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,8 +66,9 @@ async def start(bot: Client, cmd: Message):
     usr_cmd = cmd.text.split("_", 1)[-1]
     if usr_cmd == "/start":
         await add_user_to_database(bot, cmd)
-        await cmd.reply_text(
-            Config.HOME_TEXT.format(cmd.from_user.first_name, cmd.from_user.id),
+        await cmd.reply_photo(
+            photo=Config.START_PHOTO,
+            caption=Config.HOME_TEXT.format(cmd.from_user.first_name, cmd.from_user.id),
             parse_mode="Markdown",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
